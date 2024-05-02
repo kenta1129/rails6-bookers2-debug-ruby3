f861062036d82921f01849f45448a8017ca9ebd1
KentaToya <163114083+kenta1129@users.noreply.github.com>
2024-05-02 11:22:22 +0000

    [Finish]課題2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    user_path(resource)
   end
 
   def configure_permitted_parameters
