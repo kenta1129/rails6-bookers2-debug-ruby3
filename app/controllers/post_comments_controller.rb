c55d71046ec1320463a1e4b381b8fb6ff7124c71
KentaToya <163114083+kenta1129@users.noreply.github.com>
2024-05-06 13:32:29 +0000

    [Finish]応用課題６

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,13 +2,14 @@ class PostCommentsController < ApplicationController
 
 def create
     book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.book_id = book.id
+    @comment.save
 end
 
 def destroy
-    PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    @comment = PostComment.find(params[:id])
+    @comment.destroy
 end
 
   private
